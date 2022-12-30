67e348ba880b3217c5be5f77f5300bead7254785
Daniel Zhang <danielfzhang@gmail.com>
2022-12-30 17:58:38 -0500

    created BaseDNN

--- src/test.py
+++ src/test.py
@@ -1,5 +1,7 @@
 import random
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 class something:
     def __init__(self, something):
@@ -22,4 +24,26 @@ z = pd.merge(keys, y, on='keys', how='outer')
 z = pd.merge(z, x, on='keys', how='outer')
 z.index = ["red", "green", "blue"]
 
-print(z)
+# print(z)
+
+
+x = np.array([[1, 2, 3], [4, 5, 6]])
+print(np.argmax(x[0]))
+
+# X = pd.read_csv(r'./data/X_expr.csv').drop(['Unnamed: 0', 'seqLibID'], axis=1).values
+# y = pd.read_csv(r'./data/y_cog.csv').drop(['Unnamed: 0', 'seqLibID'], axis=1).values
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42, stratify=y)
+# print(np.where(y_train == 'NoCognitiveImpairment')[0].shape[0])
+# print(np.where(y_train == 'MildCognitiveImpairment')[0].shape[0])
+# print(np.where(y_train == 'AD')[0].shape[0])
+
+print(np.random.choice([1, 2, 3, 4, 5], 3, replace=False))
+
+q = [1, 2, 3]
+
+for i in q:
+    i += 1
+
+print(q)
+
